b24c3c1fec77862148ce90cbd7f4c4d8f0247787
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-10-29 20:12:06 +0100

    adding docker config

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -5,6 +5,6 @@ VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
 
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
 docker-compose -f docker-compose-build.yml build  "${IMAGE_NAME}"
-docker tag "${IMAGE_NAME}:latest" "${CI_REGISTRY}/amir-platform/${IMAGE_NAME}:${VERSION}"
-docker push "${CI_REGISTRY}/amir-platform/${IMAGE_NAME}:${VERSION}"
+docker tag "${IMAGE_NAME}:latest" "${CI_REGISTRY_IMAGE}/amir-platform/${IMAGE_NAME}:${VERSION}"
+docker push "${CI_REGISTRY_IMAGE}/amir-platform/${IMAGE_NAME}:${VERSION}"
 

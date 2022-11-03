b484668f9f5a2b2b74a600d57feb89aaa8f4c7de
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-03 03:39:08 +0100

    add documentation script

--- middlewares/S3Upload.js
+++ middlewares/S3Upload.js
@@ -2,16 +2,6 @@ const aws = require('aws-sdk')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
 
-const s3Config = {
-    bucketName: process.env.S3_BUCKET_NAME,
-    region: process.env.S3_REGION,
-    accessKeyId: process.env.S3_IAM_USER_KEY,
-    secretAccessKey: process.env.S3_IAM_USER_SECRET,
-    s3BucketEndpoint: true,
-    endpoint: process.env.S3_ENDPOINT
-
-};
-
 aws.config.update({
     accessKeyId: process.env.S3_IAM_USER_KEY,
     secretAccessKey: process.env.S3_IAM_USER_SECRET,

9f408ec4e16498e51fed1acf708b48e90dd1f02a
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-03 05:22:54 +0100

    improve cleanup

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -2,4 +2,4 @@
 pip3 install requests python-dateutil
 
 
-python3 cleanup.py $CI_SERVER_URL $GITLAB_TOKEN $CI_PROJECT_ID $GITLAB_REGISTRY_ID
+python3 ./ci/cleanup.py $CI_SERVER_URL $GITLAB_TOKEN $CI_PROJECT_ID $GITLAB_REGISTRY_ID

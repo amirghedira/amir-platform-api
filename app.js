0906ae94ef1a7721d6ce3cf3b1edb70bc338f78f
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-03 10:33:33 +0100

    adding backup db cron

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ mongosse.connect(process.env.MONGO_INFO, {
 app.use('*', (req, res, next) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
     const location = lookup(ip)
-    logger('INFO', req.method, req.originalUrl, `request accepted from ${ip} from ${location?.country || 'NA'}, ${location?.city || 'NA'}`)
+    logger('DEBUG', req.method, req.originalUrl, `request accepted from ${ip} from ${location?.country || 'NA'}, ${location?.city || 'NA'}`)
     return next()
 })
 app.use("*", (req, res, next) => {
     res.on('close', () => {
         const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
         const location = lookup(ip)
-        logger('INFO', req.method, req.originalUrl, `response close for ${ip} from ${location?.country || 'NA'}, ${location?.city || 'NA'} with status ${res.statusCode}`)
+        logger('DEBUG', req.method, req.originalUrl, `response close for ${ip} from ${location?.country || 'NA'}, ${location?.city || 'NA'} with status ${res.statusCode}`)
         req.removeAllListeners()
     })
     return next()

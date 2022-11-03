79646e652c5b8a1138fddbf6ad6151accf9d7755
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-03 04:19:21 +0100

    adding cleanup stage

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -1,8 +1,5 @@
 i=0
 
-
-
-
 for registry_tag in $(curl -X GET --header "PRIVATE-TOKEN:${GITLAB_TOKEN}"  ${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/${GITLAB_REGISTRY_ID}/tags | jq -r '.[] | @base64'); do
     i=$(( i + 1 ))
     _jq() {
@@ -10,7 +7,6 @@ for registry_tag in $(curl -X GET --header "PRIVATE-TOKEN:${GITLAB_TOKEN}"  ${CI
     }
     if [ $i -gt 5 ]
      then
-
-      curl -X DELETE --header "PRIVATE-TOKEN:${GITLAB_TOKEN}" ${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/${GITLAB_REGISTRY_ID}/$(_jq '.tag_name') >/dev/null
+      curl -X DELETE --header "PRIVATE-TOKEN:${GITLAB_TOKEN}" ${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/${GITLAB_REGISTRY_ID}/tags/$(_jq '.name') >/dev/null
     fi
 done

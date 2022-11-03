ce6d82b632ad4984af1549897b5ad7ab0d1de79e
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-03 04:21:29 +0100

    improve cleanup

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -10,3 +10,4 @@ for registry_tag in $(curl -X GET --header "PRIVATE-TOKEN:${GITLAB_TOKEN}"  ${CI
       curl -X DELETE --header "PRIVATE-TOKEN:${GITLAB_TOKEN}" ${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/${GITLAB_REGISTRY_ID}/tags/$(_jq '.name') >/dev/null
     fi
 done
+

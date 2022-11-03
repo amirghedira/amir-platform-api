b54a09cd89666f99108a10d79daec15ba67cba6a
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-03 05:21:08 +0100

    improve cleanup

--- ci/cleanup.sh
+++ ci/cleanup.sh
@@ -1,13 +1,5 @@
-i=0
 
-for registry_tag in $(curl -X GET --header "PRIVATE-TOKEN:${GITLAB_TOKEN}"  ${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/${GITLAB_REGISTRY_ID}/tags | jq -r '.[] | @base64'); do
-    i=$(( i + 1 ))
-    _jq() {
-     echo ${registry_tag} | base64 --decode | jq -r ${1}
-    }
-    if [ $i -gt 5 ]
-     then
-      curl -X DELETE --header "PRIVATE-TOKEN:${GITLAB_TOKEN}" ${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/registry/repositories/${GITLAB_REGISTRY_ID}/tags/$(_jq '.name') >/dev/null
-    fi
-done
+pip3 install requests python-dateutil
 
+
+python3 cleanup.py $CI_SERVER_URL $GITLAB_TOKEN $CI_PROJECT_ID $GITLAB_REGISTRY_ID

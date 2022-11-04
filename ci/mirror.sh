5db4f6451bf8b502b9b28cc2251fb5e34a33982d
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-04 07:50:10 +0100

    adding mirror

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -4,7 +4,7 @@ REPO_PATH="/home/centos/save-amir-platform-api"
 if [ ! -d "${REPO_PATH}" ]; then
     cd "/home/centos"
     git clone "https://amirghedira:${GITLAB_TOKEN}@gitlab.com/amir-platform/amir-platform-api.git" save-amir-platform-api
-    cd save-amir-platform-ui
+    cd save-amir-platform-api
     git remote add github "https://amirghedira:${GITHUB_TOKEN}@github.com/amirghedira/mywebsiteRestAPI.git"
 fi
 

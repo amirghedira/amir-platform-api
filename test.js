7ed999542789182188968d10289234b378e66691
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-11-07 06:14:52 +0100

    update logs volume mount

--- test.js
+++
@@ -1,4 +0,0 @@
-const url = "https://amirplatform.s3.eu-central-1.amazonaws.com/project/js7isaj4k9y03snvc49d.png"
-
-
-console.log(url.split('https://amirplatform.s3.eu-central-1.amazonaws.com/')[1])

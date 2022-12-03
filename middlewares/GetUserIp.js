e9a4e01db66cfb82be7a5e4fb6a80f32875b1be3
amirghedira06@gmail.com <amirghedira06@gmail.com>
2022-12-03 02:11:09 +0100

    check user ip

--- middlewares/GetUserIp.js
+++ middlewares/GetUserIp.js
@@ -1,4 +1,11 @@
 module.exports = (req, res, next) => {
+    console.log("header req=======")
+    console.log(req.headers)
+    console.log("==============")
+    console.log("socket req=======")
+    console.log(req.socket)
+    console.log("==============")
+
     if (req.silence)
         return next()
     if (req.headers['X-User-IP']) {
